59d725c503fee68770dc43d925489635e721b514
caglaeraslan@hotmail.com <caglaeraslan@hotmail.com>
2025-05-29 23:08:31 +0300

    29.05 gece 11

---
+++ Controllers/FavoritesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Housing.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
